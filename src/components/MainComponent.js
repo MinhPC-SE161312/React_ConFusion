a8ef9eb1fb2f9207385044ce7656417cd87584e8
MinhPC-SE161312 <105256462+MinhPC-SE161312@users.noreply.github.com>
2022-06-28 05:37:31 +0700

    Single Page Applications Part 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Contact from "./ContactComponent";
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 
 
@@ -61,6 +62,7 @@ class Main extends Component {
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
