882455da5557adeeae66ad4044c75d1546d717e1
MinhPC-SE161312 <105256462+MinhPC-SE161312@users.noreply.github.com>
2022-06-27 20:12:38 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponents.js
+++ src/components/DishdetailComponents.js
@@ -1,6 +1,6 @@
-
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 // class DishDetail extends React.Component{
 //     constructor(props){
@@ -61,9 +61,9 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 //     }
 // }
 
-function RenderDish({dish}){
+export function RenderDish({dish}){
     return(
-        <div className="col-12 col-sm-5">
+        <div className="col-12 col-sm-12">
             <Card>
                 <CardImg top src={dish.image} alt={dish.name}/>
                 <CardBody>
@@ -76,7 +76,7 @@ function RenderDish({dish}){
     );
 }
 
-function RenderComments({comments}){
+export function RenderComments({comments}){
     const comment=comments.map( (item) =>{
         return(
             <div>
@@ -87,15 +87,49 @@ function RenderComments({comments}){
     });
 
     return(
-        <div className="col-12 col-sm-7">
+        <div className="col-12 col-sm-12">
             <h2>Comments</h2>
             {comment}
         </div>
     )
 }
 
-const DishDetail = (props) => {
+class DishDetail extends React.Component{
+    constructor(props){
+        super(props);
+    }
 
+    render(){
+        if(this.props.dish!=null){
+            const dish=this.props.dish;
+            const comments=this.props.comments;
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={comments} />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else{
+            return <div></div>;
+        }
+    }
 }
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponents';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 // class Menu extends Component {
 
@@ -33,33 +33,79 @@ import DishDetail from './DishdetailComponents';
 //     }
 // }
 
-function RenderMenuItem({dish, onClick}) {
-    return(
-        <Card onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>  
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        // <Card onClick={() => onClick(dish.id)}>
+        // <CardImg width="100%" src={dish.image} alt={dish.name} />
+        // <CardImgOverlay>
+        //     <CardTitle>{dish.name}</CardTitle>
+        // </CardImgOverlay>
+        // </Card>  
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
     );
 }
 
-const Menu = (props) => {
-    const menu = props.dishes.map((dish) => {
+// const Menu = (props) => {
+//     const menu = props.dishes.map((dish) => {
+//         return (
+//             <div className="col-12 col-md-5 m-1" key={dish.id}>
+//                 <RenderMenuItem dish={dish} onClick={props.onClick} />
+//             </div>
+//         );
+//     });
+
+//     return (
+//         <div className="container">
+//             <div className="row">
+//                 {menu}
+//             </div>
+//         </div>
+//     )
+// }
+
+class Menu extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const menu = this.props.dishes.map((dish) => {
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <div key={dish.id}>
+                        <RenderMenuItem dish={dish} />
+                    </div>
+                </div>
+            );
+        });
+
         return(
-            <div className="col-12 col-md-5 m-1" key={dish.id}>
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
             </div>
         );
-    });
 
-    return(
-        <div className="container">
-            <div className="row">
-                {menu}
-            </div>
-        </div>
-    )
+    }    
+    
 }
 
 export default Menu;
